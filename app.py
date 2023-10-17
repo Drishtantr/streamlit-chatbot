7c76da36d01f38a91c604130d707a9eb58522952
Drishtantr <drishtantregmi777@gmail.com>
2023-10-17 12:07:56 -0400

    updates on agent

--- app.py
+++ app.py
@@ -1,84 +1,54 @@
-import streamlit as st
 from dotenv import load_dotenv
-import pickle
-from PyPDF2 import PdfReader
-from streamlit_extras.add_vertical_space import add_vertical_space
-
 import streamlit as st
-from dotenv import load_dotenv
-import pickle
 from PyPDF2 import PdfReader
-from streamlit_extras.add_vertical_space import add_vertical_space
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
-from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
-import os
-
-with st.sidebar:
-    st.title('🤗💬 LLM Chat App')
-    st.markdown('''
-    ## About
-    This app is an LLM-powered chatbot built using:
-    - [Streamlit](https://streamlit.io/)
-    - [LangChain](https://python.langchain.com/)
-    - [OpenAI](https://platform.openai.com/docs/models) LLM model
- 
-    ''')
-
-load_dotenv()
 
 
 def main():
-    st.header("Chat with PDF 💬")
+    load_dotenv()
+    st.set_page_config(page_title="Ask your PDF")
+    st.header("PDF Chatbot")
 
-    pdf = st.file_uploader("Upload your PDF", type='pdf')
+    # upload file
+    pdf = st.file_uploader("Upload your PDF", type="pdf")
 
-    st.write(pdf)
+    # extract the text
     if pdf is not None:
         pdf_reader = PdfReader(pdf)
-
         text = ""
         for page in pdf_reader.pages:
             text += page.extract_text()
 
-        text_splitter = RecursiveCharacterTextSplitter(
+        # split into chunks
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
             chunk_size=1000,
             chunk_overlap=200,
             length_function=len
         )
-        chunks = text_splitter.split_text(text=text)
+        chunks = text_splitter.split_text(text)
 
-        # # embeddings
-        store_name = pdf.name[:-4]
-        st.write(f'{store_name}')
+        # create embeddings
+        embeddings = OpenAIEmbeddings()
+        knowledge_base = FAISS.from_texts(chunks, embeddings)
 
-        if os.path.exists(f"{store_name}.pkl"):
-            with open(f"{store_name}.pkl", "rb") as f:
-                VectorStore = pickle.load(f)
-        else:
-            embeddings = OpenAIEmbeddings()
-            VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
-            with open(f"{store_name}.pkl", "wb") as f:
-                pickle.dump(VectorStore, f)
-
-        # embeddings = OpenAIEmbeddings()
-        # VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
-
-        # Accept user questions/query
-        query = st.text_input("Ask questions about your PDF file:")
-        # st.write(query)
-
-        if query:
-            docs = VectorStore.similarity_search(query=query, k=3)
+        # show user input
+        user_question = st.text_input("Ask a question about your PDF:")
+        if user_question:
+            docs = knowledge_base.similarity_search(user_question)
 
             llm = OpenAI()
-            chain = load_qa_chain(llm=llm, chain_type="stuff")
+            chain = load_qa_chain(llm, chain_type="stuff")
             with get_openai_callback() as cb:
-                response = chain.run(input_documents=docs, question=query)
+                response = chain.run(input_documents=docs,
+                                     question=user_question)
                 print(cb)
+
             st.write(response)
 
 
