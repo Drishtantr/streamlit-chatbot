8a54634ffa29f26c3a3f9ebe47a91ea511c51e40
Drishtantr <drishtantregmi777@gmail.com>
2023-12-11 18:47:36 -0500

    combine all

--- app.py
+++ app.py
@@ -1,102 +1,48 @@
 import streamlit as st
+from rubric_streamlit import rubric
+from pdf_chat_code import pdf_chat
+from quiz_generator import quiz
 from dotenv import load_dotenv
-from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chat_models import ChatOpenAI
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain
-from htmlTemplates import css, bot_template, user_template
-from langchain.llms import HuggingFaceHub
-
-
-def get_pdf_text(pdf_docs):
-    text = ""
-    for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-    return text
-
-
-def get_text_chunks(text):
-    text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len
-    )
-    chunks = text_splitter.split_text(text)
-    return chunks
-
-
-def get_vectorstore(text_chunks):
-    embeddings = OpenAIEmbeddings()
-    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
-    return vectorstore
-
-
-def get_conversation_chain(vectorstore):
-    llm = ChatOpenAI()
-
-    memory = ConversationBufferMemory(
-        memory_key='chat_history', return_messages=True)
-    conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm=llm,
-        retriever=vectorstore.as_retriever(),
-        memory=memory
-    )
-    return conversation_chain
-
-
-def handle_userinput(user_question):
-    response = st.session_state.conversation({'question': user_question})
-    st.session_state.chat_history = response['chat_history']
-
-    for i, message in enumerate(st.session_state.chat_history):
-        if i % 2 == 0:
-            st.write(user_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
-        else:
-            st.write(bot_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
 
 
 def main():
-    load_dotenv()
-    st.set_page_config(page_title="Ask questions to yourPDFs")
-    st.write(css, unsafe_allow_html=True)
+    st.header("Welcome to the LLM Exploration Tool!")
+    st.write("This tool provides several functionalities to explore and understand the capabilities of Large Language Models (LLMs). Select a feature from the sidebar to get started.")
 
-    if "conversation" not in st.session_state:
-        st.session_state.conversation = None
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = None
+    st.subheader("PDF Chat")
+    st.write("""
+        The 'PDF Chat' feature allows users to upload PDF documents and interact with them using a chat interface. 
+        It utilizes language models to understand and respond to queries based on the content of the uploaded PDFs.
+    """)
 
-    st.header("Ask questions about the topics")
-    user_question = st.text_input("Ask a question about your documents:")
-    if user_question:
-        handle_userinput(user_question)
+    st.subheader("Rubric Generator")
+    st.write("""
+        The 'Rubric Generator' section provides a tool for generating grading rubrics. 
+        This feature is especially useful for educators and trainers who need to create detailed and consistent assessment criteria.
+    """)
 
-    with st.sidebar:
-        st.subheader("Your documents")
-        pdf_docs = st.file_uploader(
-            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
-        if st.button("Process"):
-            with st.spinner("Processing"):
-                # get pdf text
-                raw_text = get_pdf_text(pdf_docs)
+    st.subheader("Quiz Generator")
+    st.write("""
+        The 'Quiz Generator' creates custom quizzes from provided texts. 
+        This is an ideal tool for educators and learners, facilitating the creation of educational materials and self-assessment tools.
+    """)
 
-                # get the text chunks
-                text_chunks = get_text_chunks(raw_text)
 
-                # create vector store
-                vectorstore = get_vectorstore(text_chunks)
-
-                # create conversation chain
-                st.session_state.conversation = get_conversation_chain(
-                    vectorstore)
+def render_sidebar():
+    st.sidebar.title("Navigation")
+    route = st.sidebar.radio("Go to", ["Home", "PDF_Chat", "Rubric", "Quiz"])
+    return route
 
 
 if __name__ == '__main__':
-    main()
+    load_dotenv()
+    route = render_sidebar()
+
+    if route == "PDF_Chat":
+        pdf_chat()
+    elif route == "Rubric":
+        rubric()
+    elif route == "Quiz":
+        quiz()
+    elif route == "Home":
+        main()
